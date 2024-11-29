226e28308897943b59bb056b9471db237e9be439
aryansamaria <aryansamaria44@gmail.com>
2024-11-29 15:46:14 +0530

    html_rendering

---
+++ os_path_rendering/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for os_path_rendering project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'os_path_rendering.settings')
+
+application = get_wsgi_application()
